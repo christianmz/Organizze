bff1a6322706b4749bc81680d6fbcae65246b1e2
Christian Meazza <christianmeazza@gmail.com>
2019-01-09 22:30:27 -0500

    Incomes and expenses layout

---
+++ app/src/main/java/io/github/christianmz/organizze/activities/ExpenseActivity.kt
@@ -0,0 +1,13 @@
+package io.github.christianmz.organizze.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.christianmz.organizze.R
+
+class ExpenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense)
+    }
+}
--- app/src/main/java/io/github/christianmz/organizze/activities/HomeActivity.kt
+++ app/src/main/java/io/github/christianmz/organizze/activities/HomeActivity.kt
@@ -3,6 +3,8 @@ package io.github.christianmz.organizze.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.github.christianmz.organizze.R
+import kotlinx.android.synthetic.main.activity_home.*
+import org.jetbrains.anko.startActivity
 
 class HomeActivity : AppCompatActivity() {
 
@@ -10,5 +12,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        fab_incomes.setOnClickListener { startActivity<IncomeActivity>() }
+        fab_expenses.setOnClickListener { startActivity<ExpenseActivity>() }
     }
 }
